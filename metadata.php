f4b84711c19a8d06dfa8e0f8f052ee15436444ec
alexander <coding@aggrosoft.de>
2021-05-04 16:18:19 +0200

    chore: release v1.0.4

--- metadata.php
+++ metadata.php
@@ -10,7 +10,7 @@ $aModule = array(
         'en' => 'Disable not on stock multidimensional variants'
     ],
     'thumbnail'    => '',
-    'version'      => '1.0.3',
+    'version'      => '1.0.4',
     'author'       => 'Aggrosoft GmbH',
     'extend'      => [
         \OxidEsales\Eshop\Application\Model\VariantHandler::class => \Aggrosoft\DisableNoStockVariants\Application\Model\VariantHandler::class
